3f29869809635f345273a0148dba971cfb8c3ec5
Afua-O <96188622+Afua-O@users.noreply.github.com>
2022-02-28 13:51:49 +0100

    Update R parallel plots.R

--- Saved solutions/R parallel plots.R
+++ Saved solutions/R parallel plots.R
@@ -8,34 +8,36 @@ library(plotly)
 library(tidyverse)
 library(parcoords)
 
+#########
+##Best for each user
+
 #Set working directory (Remember to change from "\" to "/")
-setwd("C:/Users/aow001/Dropbox/PhD/Work/4. Hydro-economic modelling/Python optimisation/Vol_Opt/Saved solutions")
+setwd("C:/Users/aow001/Dropbox/PhD/Work/4. Hydro-economic modelling/Python optimisation/Vol_Opt/Saved solutions/1984_clam")
 print(getwd())
 
 #data preparation#
 #Add new column called "Name"
-#For each row in the 'Name' column, put "Other solutions
+#For each row in the 'Name' column, put "Other solutions"
 # identify best solution for each objective and change the row from "Other solutions" to "Best x"
 # add two rows, one with max possible value of each obj and the other with min possible (for standardization)
 #add another row with the compromise(eg: the solution meeting target 80% of the target value)
-#if 0 is the max/best/target solution, invert that variable
-# Add additional column with labels and then a row for max possible values and another for min possible values for each y-axis
+#if 0 is the max/best/target solution, invert that variable so min can be -Max
+# Move all the "Best_X" rows to the last rows of the csv so they lay above the "other solutions" in the plot
 
-data <- read.csv("hydro_max_q2.csv", header= TRUE)
+data <- read.csv("10_solution2.csv", header= TRUE)
 head(data)
 
 #specify the preferred legend order
-data$Name <- factor(data$Name, levels = c("Best_hydropower", "Best_environment", "Best_flooding", "Best_irrigation", "Compromise", "Other solutions"))
+data$Name <- factor(data$Name, levels = c("Best hydropower", "Best environment", "Best irrigation", "Other solutions", "Min", "Max"))
  
 ggparcoord(data=data,
-           columns = c(1:4), groupColumn = 5,     
+           columns = c(4,1,3,2), groupColumn = 5,     
            scale= "uniminmax", # for no scaling use "globalminmax"
            showPoints = FALSE, 
-           title = "Trade-offs (Standardized to Min = 0 and Max = 1)",
-           alphaLines = 0.5
+           title = "Trade-offs: clam e-flows (Standardized to Min = 0 and Max = 1)",
+           alphaLines = 0.2
 ) + geom_line(size=1)+
-  #scale_color_manual(values = c(min, max, all_solutions, best_e-flows, best_hydro)) +
-  scale_color_manual(values = c("red", "green", "blue", "orange", "grey", "grey", "grey")) +
+  scale_color_manual(values = c("#CC0033", "#00CC99", "#6600FF", "#E8E8E8", "#E8E8E8", "#E8E8E8")) +
   theme_ipsum()+
   theme(
     legend.position= "right",#"none",
@@ -43,6 +45,36 @@ ggparcoord(data=data,
     plot.title = element_text(size=10)
   ) +
   xlab("") +
-  ylab("Direction of preference ¡÷" )
+  ylab("Direction of preference â†’") +
+  theme(axis.title.y = element_text(hjust=0.95, size = 11)) 
 
-######
+##########
+###See if there is compromise- 80%
+#Save the solutions file as a new file
+#Highlight 80% e-flows threshold an 80th percentile hydropower
+#change the Name column into 3 categories: "Eflow" for the solutions meeting 80% for the e-flows column, 'Hydropower' for the solutions above 80th percentile of the hydropower and leave the rest as Other solutions for the res
+
+
+data <- read.csv("10_solution3.csv", header= TRUE)
+head(data)
+
+#specify the preferred legend order
+data$Name <- factor(data$Name, levels = c("Hydropower", "Eflow", "Other solutions", "Min", "Max"))
+
+ggparcoord(data=data,
+           columns = c(4,1,3,2), groupColumn = 5,     
+           scale= "uniminmax", # for no scaling use "globalminmax"
+           showPoints = FALSE, 
+           title = "Trade-offs: clam e-flows (Standardized to Min = 0 and Max = 1)",
+           alphaLines = 0.2
+) + geom_line(size=1)+
+  scale_color_manual(values = c("#CC0033", "#33CCCC", "#E8E8E8", "#E8E8E8", "#E8E8E8")) +
+  theme_ipsum()+
+  theme(
+    legend.position= "right",#"none",
+    legend.title = element_blank(),
+    plot.title = element_text(size=10)
+  ) +
+  xlab("") +
+  ylab("Direction of preference â†’" ) +
+  theme(axis.title.y = element_text(hjust=0.95, size = 11)) 
